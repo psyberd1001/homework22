b3ddec7034cd2d0c013093cc0af0476323b44d51
psyberd1001 <108468415+psyberd1001@users.noreply.github.com>
2024-05-02 20:52:50 +0300

    Add files via upload

--- homework22.py
+++ homework22.py
@@ -1,10 +1,12 @@
 class Buiding:
     def __init__(self):
         self.numberOfFloors = 10
-        self.buildingType = '11'
+        self.buildingType = '10'
 
-    def __eq__(self):
-        return self.numberOfFloors == self.buildingType
+    def __eq__(self, other):
+        return self.numberOfFloors == other.numberOfFloors and self.buildingType == other.buildingType
+
 
 buiding = Buiding()
-print(buiding.__eq__())
+
+print(buiding)
